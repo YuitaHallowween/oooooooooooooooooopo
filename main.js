11a607fb82a0d810418c71dda7d33e782c26a48c
YuitaHallowween <89964085+YuitaHallowween@users.noreply.github.com>
2021-09-02 11:59:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+<canvas id="myCanvas" width="800" height="400">
+</canvas>
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+greencar_width = 75;
+greencar_height = 100;
+
+function add() {
+	background_imageTag = new Image();
+	background_imageTag.onload = uploadBackground;
+	background_imageTag.scr = background_image;
+
+	greencar_imgTag = new Image();
+	greencar_imgTag.onload = uploadgreencar;
+	greencar_imgTag.scr = greencar_image;
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x, greencar_y, greencar_width, greencar_height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if(greencar_y >=0)
+	{
+		greencar_y = greencar_y - 10;
+		console.log("when up arrow is pressed, x = " + greencar_x + " y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function down()
+{
+	if(greencar_y >=500)
+	{
+		greencar_y = greencar_y + 10;
+		console.log("when down arrow is pressed, x = " + greencar_x + " y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function left()
+{
+	if(greencar_y >=0)
+	{
+		greencar_y = greencar_y - 10;
+		console.log("when left arrow is pressed, x = " + greencar_x + " y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function right()
+{
+	if(greencar_y >=700)
+	{
+		greencar_y = greencar_y + 10;
+		console.log("when left arrow is pressed, x = " + greencar_x + " y = " + greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
